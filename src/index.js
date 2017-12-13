b5dc8b8523521e5984c47a37c91fd703d8d9c598
razevedo <razevedo@TF-1933>
2017-12-13 10:46:55 -0200

    Test YTSearch API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 // Youtube API Key
 const API_KEY = 'AIzaSyAVvQ2yZtJchp28liiNi6mkDkRxZgQDGMM';
 
+YTSearch({key:API_KEY,term:'surfboards'}, function(data){
+    console.log(data);
+});
+
+// The most parent component shoul be responsible for fetch data.
 // Create a new component.This component should produce some HTML
 const App  = () => {
     return (
