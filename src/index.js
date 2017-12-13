bd2a41f1c04f3e50363cbd3614d4c7d88267e7b6
razevedo <razevedo@TF-1933>
2017-12-13 11:01:51 -0200

    Refactoring Functional App Component to Class Components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
 // Youtube API Key
-const API_KEY = 'AIzaSyAVvQ2yZtJchp28liiNi6mkDkRxZgQDGMM';
-
-YTSearch({key:API_KEY,term:'surfboards'}, function(data){
-    console.log(data);
-});
+const API_KEY = "AIzaSyAVvQ2yZtJchp28liiNi6mkDkRxZgQDGMM";
 
 // The most parent component shoul be responsible for fetch data.
 // Create a new component.This component should produce some HTML
-const App  = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // YTSearch({ key: API_KEY, term: "surfboards" }, (data) => {
+    //     this.setState({videos:data});
+    // });
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+       this.setState({ videos });
+    // this.setState({ videos: videos });
+      });
+
+  }
+  render() {
     return (
-        <div>
-            <SearchBar />
-        </div>
+      <div>
+        <SearchBar />
+      </div>
     );
+  }
 }
 
 // Take this component's generataed HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />,document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
